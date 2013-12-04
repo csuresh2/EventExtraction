85fb6a3642274b27168999f28d580308a6a58fe3
Chethan Suresh <chethans@myhpenvy.(none)>
2013-12-03 22:47:24 -0600

    update

--- src/TwitterEvents/TwitterMonitor.java
+++ src/TwitterEvents/TwitterMonitor.java
@@ -1,42 +1,85 @@
 package TwitterEvents;
 
+import java.io.FileWriter;
 import java.io.IOException;
 
-import twitter4j.*;
-import java.io.*;
+import twitter4j.FilterQuery;
+import twitter4j.StallWarning;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.TwitterException;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.json.DataObjectFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import au.com.bytecode.opencsv.CSVWriter;
 
 class TwitterMonitor {
 
 	public static void main(String[] args) throws TwitterException, IOException{
-		StatusListener listener = new StatusListener(){
+		StatusListener listener = new StatusListener() {
 			// Create file 
-			FileWriter fstream = new FileWriter("out5.txt");
-			BufferedWriter dump = new BufferedWriter(fstream);
+			CSVWriter writer = new CSVWriter(new FileWriter("/home/chethans/twitterOutput.csv"));
+			HashMap<Long, StringBuilder> map = new HashMap<Long, StringBuilder>();
+			
 			public void onStatus(Status status) {
-				System.out.println(status.getUser().getName() + " : " + status.getText());
+				
 				try {
-					dump.write("\""+status.getCreatedAt()+"\"" + "," + "\""+status.getUser().getName()+"\""+ "," + status.getGeoLocation().getLatitude()+ "," + status.getGeoLocation().getLongitude() + "," + "\"" + status.getText()+"\"");
-					dump.newLine();
-					dump.flush();
-				} catch (IOException e) {
+					
+					if(status.getUser().getLang().equals("en")) {
+						System.out.println(map.size());
+						if(!map.containsKey(status.getId())) {
+							StringBuilder csvOutputString = new StringBuilder();
+							csvOutputString.append(status.getId() + "#@XF" + status.getUser()
+								.getScreenName() + "#@XF" + status.getText() + "#@XF" + "0" +
+								"#@XF" + DataObjectFactory.getRawJSON(status));
+							map.put(status.getId(), csvOutputString);
+						}
+					}
+					
+					if(map.size() > 100) {
+						// Write the map contents to the csv file
+						for(Map.Entry<Long, StringBuilder> entry: map.entrySet()) {
+							String[] data = entry.getValue().toString().split("#@XF");
+							writer.writeNext(data);
+						}
+						
+						finalize();
+						System.exit(0);
+					}
+				} catch (Throwable e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
+			
 			public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {}
 			public void onTrackLimitationNotice(int numberOfLimitedStatuses) {}
 			public void onException(Exception ex) {
 				ex.printStackTrace();
 			}
+			
 			@Override
 			public void onScrubGeo(long arg0, long arg1) {
 				// TODO Auto-generated method stub
-
+				
 			}
 			@Override
 			public void onStallWarning(StallWarning arg0) {
 				// TODO Auto-generated method stub
-
+				System.out.println("Stall Warning: " + arg0.getMessage());
+			}
+			
+			@Override
+			protected void finalize() throws Throwable {
+				super.finalize();
+				/*dump.close();
+				fstream.close();*/
+				writer.close();
 			}
 		};
 		TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
@@ -47,15 +90,16 @@ class TwitterMonitor {
 		twitterStream.addListener(listener);
 
 		// Filter by terms
-		FilterQuery filtre = new FilterQuery();
+		FilterQuery filter = new FilterQuery();
 		String[] keywordsArray = { "#event"};
 		//do0uble[][] locations = {{-88.295574,40.083851},{-88.188114,40.125866}}; 
-		//filtre.locations(locations);
-		filtre.track(keywordsArray);
-		twitterStream.filter(filtre);
+		//filter.locations(locations);
+		filter.track(keywordsArray);
+		twitterStream.filter(filter);
 
-		// sample() method internally creates a thread which manipulates TwitterStream and calls these adequate listener methods continuously.
-		//twitterStream.sample();
+		// sample() method internally creates a thread which manipulates TwitterStream and 
+		// calls these adequate listener methods continuously.
+		twitterStream.sample();
 	}
 
 }
